a342f8bd2e6c26c42cab8775d5384a7b7d24557d
anhnguyen6535 <anhnguyen6535@gmail.com>
2023-11-17 22:14:52 -0700

    style card

--- order-system-ionic/src/components/FoodItemCards/TextCard.tsx
+++ order-system-ionic/src/components/FoodItemCards/TextCard.tsx
@@ -1,13 +1,14 @@
 import { IonItem, IonLabel, IonNote } from "@ionic/react";
+import css from "../../styles/TextCard.module.css"
 
 export default function TextCard(props: TextCardProps) {
   const validLinesValues = ['none', 'full', 'inset', 'inset-x'];
   const lines = validLinesValues.includes(props.lines || '') ? props.lines : 'full';
 
   return (
-    <IonItem lines={lines as 'none' | 'full' | 'inset' | undefined} style={{ fontSize: '20.0px' }}>
+    <IonItem lines={lines as 'none' | 'full' | 'inset' | undefined} className={css.ionItem}>
       <IonLabel>{props.label}</IonLabel>
-      <IonNote slot='end' style={{ fontSize: '20.0px', color: '#ff4961' }}>
+      <IonNote slot='end'>
         ${props.note}
       </IonNote>
     </IonItem>
