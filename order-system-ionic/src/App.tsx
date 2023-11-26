371c465c4c62887d114476e15444b158febf7999
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2023-11-26 16:19:12 -0700

    fix main merge conflicts

--- order-system-ionic/src/App.tsx
+++ order-system-ionic/src/App.tsx
@@ -38,7 +38,6 @@ import "@ionic/react/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
-import NavBar from "./components/NavBar";
 import routes from "./route-config";
 
 setupIonicReact();
