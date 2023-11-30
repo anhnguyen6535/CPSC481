fa54cb3f10348a9363602af6984901b35da349c2
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2023-11-30 15:41:34 -0700

    fix welcome screen

--- order-system-ionic/src/components/QRScanRedirect.tsx
+++ order-system-ionic/src/components/QRScanRedirect.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import Welcome from "../pages/Welcome";
+import Welcome from "../pages/Welcome/Welcome";
 
 const QRScanRedirect: React.FC<QRRedirectProps> = ({ children }) => {
   const location = useLocation();
 
-  return location.pathname != "/welcome" ? <>{children}</> : <Welcome />;
+  return location.pathname == "/welcome" || location.pathname == "/" ? (
+    <Welcome />
+  ) : (
+    <>{children}</>
+  );
 };
 
 export default QRScanRedirect;
