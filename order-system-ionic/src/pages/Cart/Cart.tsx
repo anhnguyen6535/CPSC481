d293e7bd38930fba3f9c21335dae136b5b792c21
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2023-12-01 15:15:04 -0700

    handle case when no items in cart

--- order-system-ionic/src/pages/Cart/Cart.tsx
+++ order-system-ionic/src/pages/Cart/Cart.tsx
@@ -9,6 +9,7 @@ import { selectCartData } from "../../redux/selectors/cartSelectors";
 import { CardTypeEnum } from "../../components/FoodItemCards/MenuFoodItemCard/MenuFoodItemCard";
 import styles from "./Cart.module.scss";
 import { useHistory } from "react-router";
+import cartImage from "../../../assets/cart.png";
 
 const Cart: React.FC = () => {
   const history = useHistory();
@@ -25,53 +26,84 @@ const Cart: React.FC = () => {
     setSubtotal(calculatedSubtotal);
   }, [cartData]);
 
+  const handleRedirectToHomePage = () => {
+    history.push("/home");
+  };
+
   return (
     <Layout pageTitle="Your Order" backButton={true}>
-      {cartData.items.map((foodItem) => (
-        <MenuFoodItemCard
-          item={foodItem.item}
-          amount={foodItem.quantity}
-          type={CardTypeEnum.CART}
-        />
-      ))}
+      {cartData.items.length > 0 ? (
+        <>
+          {cartData.items.map((foodItem) => (
+            <MenuFoodItemCard
+              item={foodItem.item}
+              amount={foodItem.quantity}
+              type={CardTypeEnum.CART}
+            />
+          ))}
 
-      <div className="ion-text-center">
-        <IonButton fill="clear" onClick={() => history.push("/home")}>Add More Items</IonButton>
-      </div>
+          <div className="ion-text-center">
+            <IonButton fill="clear" onClick={handleRedirectToHomePage}>
+              Add More Items
+            </IonButton>
+          </div>
 
-      <IonItemDivider />
+          <IonItemDivider />
 
-      {cartData.items.map((foodItem, index) => (
-        <TextCard
-          key={foodItem.item.id}
-          lines={index === cartData.items.length - 1 ? "" : "none"} // Line only for the last item
-          label={`${foodItem.quantity}x ${foodItem.item.name}`}
-          note={`${(foodItem.item.price * foodItem.quantity).toFixed(2)}`}
-          noteColor="black"
-        />
-      ))}
-      <TextCard
-        lines="none"
-        label="Subtotal"
-        note={`${subtotal.toFixed(2)}`}
-        noteColor="black"
-      />
-      <TextCard
-        label="Tax"
-        note={`${(subtotal * taxRate).toFixed(2)}`}
-        noteColor="black"
-      />
-      <TextCard
-        lines="none"
-        label="Total"
-        note={`${(subtotal * (1 + taxRate)).toFixed(2)}`}
-        noteColor="black"
-        fontWeight="bold"
-      />
+          {cartData.items.map((foodItem, index) => (
+            <TextCard
+              key={foodItem.item.id}
+              lines={index === cartData.items.length - 1 ? "" : "none"} // Line only for the last item
+              label={`${foodItem.quantity}x ${foodItem.item.name}`}
+              note={`${(foodItem.item.price * foodItem.quantity).toFixed(2)}`}
+              noteColor="black"
+            />
+          ))}
+          <TextCard
+            lines="none"
+            label="Subtotal"
+            note={`${subtotal.toFixed(2)}`}
+            noteColor="black"
+          />
+          <TextCard
+            label="Tax"
+            note={`${(subtotal * taxRate).toFixed(2)}`}
+            noteColor="black"
+          />
+          <TextCard
+            lines="none"
+            label="Total"
+            note={`${(subtotal * (1 + taxRate)).toFixed(2)}`}
+            noteColor="black"
+            fontWeight="bold"
+          />
 
-      <div className="ion-text-center">
-        <IonButton className={styles.placeOrderButton}>Place Order</IonButton>
-      </div>
+          <div className="ion-text-center">
+            <IonButton className={styles.placeOrderButton}>
+              Place Order
+            </IonButton>
+          </div>
+        </>
+      ) : (
+        <div className={styles.noCartItems}>
+          <div>
+            <div>
+              <img
+                src={cartImage}
+                alt="Cart is empty"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
+            </div>
+            <p>There are no items in your cart.</p>
+          </div>
+          <IonButton
+            onClick={handleRedirectToHomePage}
+            className={styles.noCartItemsButton}
+          >
+            Add Items Now
+          </IonButton>
+        </div>
+      )}
     </Layout>
   );
 };
