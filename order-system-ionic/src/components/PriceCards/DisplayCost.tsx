0641f3afd0d4885cc729421860f87f581f0a9f8b
Anh Nguyen <63438267+anhnguyen6535@users.noreply.github.com>
2023-12-02 00:48:00 -0700

    refractor cart & create display cost component

---
+++ order-system-ionic/src/components/PriceCards/DisplayCost.tsx
@@ -0,0 +1,36 @@
+import TextCard from "../FoodItemCards/TextCard";
+
+type DisplayCostProps = {
+    subtotal: number;
+};
+
+const DisplayCost: React.FC<DisplayCostProps> = ({subtotal}) => {
+    // fake data
+    const taxRate = 0.05;
+
+
+    return(
+        <>
+            <TextCard
+                lines="none"
+                label="Subtotal"
+                note={`${subtotal.toFixed(2)}`}
+                noteColor="black"
+            />
+            <TextCard
+                label="Tax"
+                note={`${(subtotal * taxRate).toFixed(2)}`}
+                noteColor="black"
+            />
+            <TextCard
+                lines="none"
+                label="Total"
+                note={`${(subtotal * (1 + taxRate)).toFixed(2)}`}
+                noteColor="black"
+                fontWeight="bold"
+            />
+        </>
+    )
+}
+
+export default DisplayCost;
--- order-system-ionic/src/pages/Cart/Cart.tsx
+++ order-system-ionic/src/pages/Cart/Cart.tsx
@@ -11,12 +11,12 @@ import styles from "./Cart.module.scss";
 import { useHistory } from "react-router";
 import cartImage from "../../../assets/cart.png";
 import Divider from "../../components/Divider/Divider";
+import DisplayCost from "../../components/PriceCards/DisplayCost";
 
 const Cart: React.FC = () => {
   const history = useHistory();
   const [subtotal, setSubtotal] = useState(0.0);
-  // fake data
-  const taxRate = 0.05;
+
   const cartData = useTypedSelector(selectCartData);
 
   useEffect(() => {
@@ -50,35 +50,7 @@ const Cart: React.FC = () => {
             </IonButton>
           </div>
 
-          <Divider />
-
-          {cartData.items.map((foodItem, index) => (
-            <TextCard
-              key={foodItem.item.id}
-              lines={index === cartData.items.length - 1 ? "" : "none"} // Line only for the last item
-              label={`${foodItem.quantity}x ${foodItem.item.name}`}
-              note={`${(foodItem.item.price * foodItem.quantity).toFixed(2)}`}
-              noteColor="black"
-            />
-          ))}
-          <TextCard
-            lines="none"
-            label="Subtotal"
-            note={`${subtotal.toFixed(2)}`}
-            noteColor="black"
-          />
-          <TextCard
-            label="Tax"
-            note={`${(subtotal * taxRate).toFixed(2)}`}
-            noteColor="black"
-          />
-          <TextCard
-            lines="none"
-            label="Total"
-            note={`${(subtotal * (1 + taxRate)).toFixed(2)}`}
-            noteColor="black"
-            fontWeight="bold"
-          />
+          <DisplayCost subtotal={subtotal} />
 
           <div className="ion-text-center">
             <IonButton className={styles.placeOrderButton}>
