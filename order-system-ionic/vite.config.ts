c2b87489e52545e160ea86d97ea58f7ec965b2da
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2023-12-03 12:33:10 -0700

    update vite config

--- order-system-ionic/vite.config.ts
+++ order-system-ionic/vite.config.ts
@@ -2,12 +2,9 @@ import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(),legacy()],
+  base: '/CPSC481/', // Add this line
   test: {
     globals: true,
     environment: 'jsdom',
