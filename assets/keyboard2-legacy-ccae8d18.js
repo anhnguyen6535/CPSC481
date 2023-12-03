48745cd319b8ce885e1ca2b170351982919983b5
Anh Nguyen <63438267+anhnguyen6535@users.noreply.github.com>
2023-12-02 22:14:50 -0700

    Updates

--- assets/keyboard2-legacy-ccae8d18.js
+++ assets/keyboard2-legacy-ccae8d18.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fffec045.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-607f7505.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- assets/swipe-back-legacy-5b0b0a03.js
+++ assets/swipe-back-legacy-5b0b0a03.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-fffec045.js","./index3-legacy-ca87bc71.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
+System.register(["./index-legacy-607f7505.js","./index3-legacy-c3385c5d.js"],(function(e,t){"use strict";var n,r,i;return{setters:[e=>{n=e.i,r=e.c},e=>{i=e.createGesture}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
