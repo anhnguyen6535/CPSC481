4bd631e85dbc2d8bec21c8d2229d16503900f993
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2023-12-03 12:53:39 -0700

    update base path

--- order-system-ionic/vite.config.ts
+++ order-system-ionic/vite.config.ts
@@ -1,13 +1,18 @@
-import legacy from '@vitejs/plugin-legacy'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+// Use "path.resolve" where you need it
+const { resolve } = path;
+
 export default defineConfig({
-  plugins: [react(),legacy()],
-  base: '/CPSC481/',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        404: resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
+});
