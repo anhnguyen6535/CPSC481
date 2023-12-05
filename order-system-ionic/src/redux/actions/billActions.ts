5292f28da9d447975f62933298a11f96c3ef3d76
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2023-12-05 16:41:39 -0700

    store split bill diners in redux

--- order-system-ionic/src/redux/actions/billActions.ts
+++ order-system-ionic/src/redux/actions/billActions.ts
@@ -1,9 +1,19 @@
 import { BillActionTypes } from "../actionTypes";
 
 export const orderBill = () => ({
-    type: BillActionTypes.ORDER_BILL,
+  type: BillActionTypes.ORDER_BILL,
 });
 
 export const resetBill = () => ({
-    type: BillActionTypes.RESET_BILL,
+  type: BillActionTypes.RESET_BILL,
+});
+
+export const addDiner = (name: string) => ({
+  type: BillActionTypes.ADD_DINER,
+  payload: name,
+});
+
+export const removeDiner = (name: string) => ({
+  type: BillActionTypes.REMOVE_DINER,
+  payload: name,
 });
--- order-system-ionic/src/redux/selectors/billSelectors.ts
+++ order-system-ionic/src/redux/selectors/billSelectors.ts
@@ -1,4 +1,5 @@
 import { RootState } from "../store";
 
-export const selectIsBillOrdered = (state: RootState) =>
-  state.bill.billOrdered;
+export const selectIsBillOrdered = (state: RootState) => state.bill.billOrdered;
+export const selectSplitBillDiners = (state: RootState) =>
+  state.bill.splitBillDiners;
