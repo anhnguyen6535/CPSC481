16f54715f2200ef118531e0d61614fe786763205
Alan A <58347931+AlanAyy@users.noreply.github.com>
2023-12-05 17:17:00 -0700

    Fixed Visual Bug in OrderFoodItemCard.tsx

--- order-system-ionic/src/components/FoodItemCards/OrderFoodItemCard/OrderFoodItemCard.tsx
+++ order-system-ionic/src/components/FoodItemCards/OrderFoodItemCard/OrderFoodItemCard.tsx
@@ -1,16 +1,19 @@
-import { IonCard, IonCardHeader, IonCardTitle } from "@ionic/react";
+import {
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+} from "@ionic/react";
 import { getFoodImageUri } from "../../../../data/menuItems/utils";
 import { MenuItem } from "../../../types";
 import { formatPrice } from "../Utils";
 import styles from "./OrderFoodItemCard.module.scss";
 
-
 interface CardProps {
     item: MenuItem;
     amount: number;
-};
+}
 
-const OrderFoodItemCard: React.FC<CardProps> = ({ 
+const OrderFoodItemCard: React.FC<CardProps> = ({
     item,
     amount,
 }) => {
@@ -18,14 +21,16 @@ const OrderFoodItemCard: React.FC<CardProps> = ({
         <IonCard className={styles.ionCardContainer}>
             <div className={styles.divContent} >
                 <img src={getFoodImageUri(item.imagePath)} alt={item.name} className={styles.cardImage} />
-                
+
                 <div className={styles.contentContainer}>
-                    <IonCardHeader style={{ flexWrap: 'wrap' }}>
-                        <IonCardTitle className={styles.foodName}>{item.name}</IonCardTitle>
-                        <IonCardTitle className={styles.foodName}>Quantity: {amount}</IonCardTitle>
+                    <IonCardHeader style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <IonCardTitle className={styles.foodName}>{item.name}</IonCardTitle>
+                            <IonCardTitle className={styles.foodName}>Quantity: {amount}</IonCardTitle>
+                        </div>
+                        <IonCardTitle style={{ justifyContent: 'flex-end' }}>{formatPrice(item.price * amount)}</IonCardTitle>
                     </IonCardHeader>
-                    <IonCardTitle className={styles.price}>{formatPrice(item.price * amount)}</IonCardTitle>
-                </div>       
+                </div>
             </div>
         </IonCard>
     );
