3c5e85174fcf84d43db6d971ec01a5c84ab0275c
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2023-12-05 17:39:13 -0700

    logic for getting orders for each person

--- order-system-ionic/src/redux/selectors/billSelectors.ts
+++ order-system-ionic/src/redux/selectors/billSelectors.ts
@@ -1,22 +1,55 @@
+import { CartItem } from "../../types";
 import { PersonOrder } from "../reducers/billReducer";
+import { OrderState } from "../reducers/orderReducer";
 import { RootState } from "../store";
 
 export const selectIsBillOrdered = (state: RootState) => state.bill.billOrdered;
 export const selectSplitBillDiners = (state: RootState) =>
   state.bill.splitBillDiners;
 
-export const getItemsForPerson = (state: RootState, personName: string) => {
-  const { bill } = state;
-  const personOrder: PersonOrder = {
-    personName,
-    selectedItems: [],
-  };
+export const selectOrderByPerson = (state: RootState) =>
+  state.bill.selectedItemsByPerson;
 
-  bill.splitBillItems.forEach((item) => {
-    if (item.selectedPeople.includes(personName)) {
-      personOrder.selectedItems.push(item);
-    }
+export const selectSplitBillOrders = (state: RootState) => {
+  const { bill, order } = state;
+
+  return bill.splitBillDiners.map((personName) => {
+    const selectedItems = bill.selectedItemsByPerson[personName] || [];
+
+    const personOrder = {
+      personName,
+      selectedItems: selectedItems
+        .map((selectedItem) => {
+          const orders = order.orders.find((o) =>
+            o.items.find(
+              (cartItem: CartItem) => cartItem.item.id === selectedItem.itemId
+            )
+          );
+
+          if (orders) {
+            const selectedItemOrder = orders.items.find(
+              (cartItem: CartItem) => cartItem.item.id === selectedItem.itemId
+            );
+
+            const totalForSelectedItem = selectedItemOrder
+              ? (selectedItemOrder.quantity * selectedItemOrder.item.price) /
+                selectedItem.selectedPeople.length
+              : 0;
+
+            return {
+              item: selectedItemOrder ? selectedItemOrder.item : null,
+              quantity: selectedItemOrder
+                ? selectedItemOrder.quantity /
+                  selectedItem.selectedPeople.length
+                : 0,
+              totalPrice: totalForSelectedItem,
+            };
+          }
+          return null;
+        })
+        .filter((item) => item !== null),
+    };
+
+    return personOrder;
   });
-
-  return personOrder;
 };
